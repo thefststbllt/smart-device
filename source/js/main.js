55eff42beb31be39bde31310cd5bc5b40fed91f4
thefststbllt <bel12pasha@yandex.ru>
2022-11-03 22:32:37 +0300

    Very final fixes

--- source/js/main.js
+++ source/js/main.js
@@ -4,20 +4,13 @@ import {scrollToForm} from './modules/scroll/scroll';
 import {addAccordion} from './modules/accordion/accordion';
 import {toggleText} from './modules/text-more/text-more';
 
-// ---------------------------------
 window.addEventListener('DOMContentLoaded', () => {
 
-  // Utils
-  // ---------------------------------
   iosVhFix();
-
-  // Modules
-  // ---------------------------------
   scrollToForm();
   addAccordion();
   toggleText();
 
-  // ---------------------------------
   window.addEventListener('load', () => {
     initModals();
   });
--- source/js/utils/ios-checker.js
+++ source/js/utils/ios-checker.js
@@ -1,7 +1,5 @@
 import {PLATFORMS} from './platforms';
 
 export const iosChecker = () => {
-  return PLATFORMS.includes(navigator.platform)
-  // iPad on iOS 13 detection
-  || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
+  return PLATFORMS.includes(navigator.platform) || (navigator.userAgent.includes('Mac') && 'ontouchend' in document);
 };
